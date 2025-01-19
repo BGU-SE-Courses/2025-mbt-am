609ea3924adea108902fbf50769a85f4a7f8f0fc
noash <shvetsn@post.bgu.ac.il>
2025-01-19 20:29:50 +0200

    Admin - few new function

--- Cucumber/src/test/java/hellocucumber/OpenCartActuatorAdmin.java
+++ Cucumber/src/test/java/hellocucumber/OpenCartActuatorAdmin.java
@@ -1,9 +1,6 @@
 package hellocucumber;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Point;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -11,38 +8,64 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 //import org.junit.Assert;
 
-
 public class OpenCartActuatorAdmin {
-
-    protected WebDriver driver;
+    private WebDriver driver;
     private WebDriverWait wait;
 
-    public void initSessionAsAdmin(String webDriver, String path) {
-        System.setProperty(webDriver, path);
+    public OpenCartActuatorAdmin() {
+        String driverPath = "Selenium/chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", driverPath);
 
-        // new chrome driver object
-        this.driver = new ChromeDriver();
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
 
-        // new web driver wait -> waits until element are loaded (40 sec max)
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(40));
+    public void openCartAdmin() throws InterruptedException {
+        // Navigate to the OpenCart website
+        driver.get("http://localhost/opencartsite/admin/");
+        Thread.sleep(1000);
+    }
 
-
-        // launch website -> localhost
-        driver.get("http://localhost/OpenCartFile/");
-
+    public void enlargeWindow(){
         // maximize the window - some web apps look different in different sizes
-        driver.manage().window().setPosition(new Point(0, 0));
+        driver.manage().window().maximize();
+
     }
 
-    public void enterLoginInfoAdmin(String username, String password) {
-        // locate the username input box and enter username
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='input-username']"))).sendKeys(username);
-        // locate the password input box and enter password
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='input-password']"))).sendKeys(password);
-        // locate Log in button and press
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[1]"))).click();
-        wait.until(ExpectedConditions.titleContains("Dashboard"));
+    public void LogInToAdmin(String userName, String password) throws InterruptedException {
+        WebElement nameElement = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='input-username']")));
+        nameElement.sendKeys(userName);
+
+        // Wait until the review input is present and interactable
+        WebElement passwordElement = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='input-password']")));
+        passwordElement.sendKeys(password);
+
+        // Wait until the "Agree" checkbox or button is clickable and click it
+        WebElement continueCheckbox = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[1]")));
+        continueCheckbox.click();
+
+        // Wait until the "Agree" checkbox or button is clickable and click it
+        WebElement exitElement = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"modal-security\"]/div/div/div[1]/button")));
+        exitElement.click();
+
     }
 
 
+//    public void scrollToElement(By elementLocator) {
+//        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(elementLocator));
+//        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+//        // Wait for a moment
+//        try {
+//            Thread.sleep(500);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+
+    public void closeBrowser() throws InterruptedException {
+        // Close the browser
+        Thread.sleep(5000);
+        driver.quit();
+    }
 }
